eed7f6b0ca12de60452b74da6ff46ba3eca439d1
Bazio <baziovvip@gmail.com>
2022-10-11 13:09:02 -0700

    first commit

---
+++ spec/generator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GeneratorView from '../lib/generator-view';
+
+describe('GeneratorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
